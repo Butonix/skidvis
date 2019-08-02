2d73d2ee97e2936abe447c777e0d8c9ee5061579
cvaize <cvaize@gmail.com>
2019-08-02 23:08:55 +0500

    (Feat) Organization

--- client/components/ReviewEdit.vue
+++ client/components/ReviewEdit.vue
@@ -5,7 +5,10 @@
         <avatar
           :user="user"
         />
-        <div v-if="user.f_name && user.m_name" class="pl-2 ml-1 font-weight-bolder" v-text="user.f_name + ' ' + user.m_name"/>
+        <div v-if="user.f_name && user.l_name" class="pl-2 ml-1 font-weight-bolder" v-text="user.f_name + ' ' + user.l_name.trim().charAt(0).toUpperCase() + '.'"/>
+        <div v-else-if="user.f_name" class="pl-2 ml-1 font-weight-bolder" v-text="user.f_name"/>
+        <div v-else-if="user.email" class="pl-2 ml-1 font-weight-bolder" v-text="user.email"/>
+        <div v-else class="pl-2 ml-1 font-weight-bolder" v-text="'АК'"/>
       </div>
 
       <div class="d-flex align-items-center justify-content-end">
